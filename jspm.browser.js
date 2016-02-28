0c97a68e3fa65970722641fecd752fc45aaca3ce
ryanwischkaemper <rwischka@gmail.com>
2016-02-28 15:14:43 -0600

    Fix build issue

--- jspm.browser.js
+++ jspm.browser.js
@@ -2,5 +2,19 @@ SystemJS.config({
   paths: {
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
+  },
+  bundles: {
+    "dist/oidc-token-manager.bundled.js": [
+      "github:frankwallis/plugin-typescript@4.0.1.json",
+      "npm:systemjs-plugin-babel@0.0.6.json",
+      "npm:systemjs-plugin-babel@0.0.6/babel-helpers/classCallCheck.js",
+      "npm:systemjs-plugin-babel@0.0.6/babel-helpers/createClass.js",
+      "src/FrameLoader.ts",
+      "src/OidcClient.ts",
+      "src/OidcTokenManager.ts",
+      "src/Token.ts",
+      "src/Utils.ts",
+      "src/index.ts"
+    ]
   }
 });
