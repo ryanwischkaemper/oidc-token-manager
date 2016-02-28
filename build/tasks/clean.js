d0d04f6d7f99b59df59de0af4896a3bdf75e2b3f
ryanwischkaemper <rwischka@gmail.com>
2016-02-28 10:19:52 -0600

    Add systemjs debug helpers

--- build/tasks/clean.js
+++ build/tasks/clean.js
@@ -4,6 +4,7 @@ var del = require('del');
 var vinylPaths = require('vinyl-paths');
 
 gulp.task('clean', function() {
-  return gulp.src([paths.output,paths.outputSample + '*.js'])
+  return gulp.src([paths.output, paths.outputSample + '*.js',
+  '!' + paths.outputSample + 'system.tap.js', '!' + paths.outputSample + 'system.yuml.js'])
     .pipe(vinylPaths(del));
 });
